50330e820e9a4651bd97295eadec77ed337a64e3
Faireum-Eric <ericzhang@faireum.io>
2018-12-05 17:36:00 +0800

    add a eosinit config

---
+++ src/common/js/aosInit.js
@@ -0,0 +1,21 @@
+import AOS from "libs/aos/aos";
+
+$(function(){
+    AOS.init({
+        offset: 0,
+        duration: 300,
+        easing: "ease",
+        delay: 0,
+        anchor: 'body',
+        disable: "mobile",
+        once: false
+    });
+});
+
+$(window).on('scroll', function() {
+    AOS.refreshHard();
+})
+
+$(window).on('click', function() {
+    AOS.refresh();
+})
