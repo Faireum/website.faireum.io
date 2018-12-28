b4138ff80dd904ddcf086405d3ef620a7fceabee
Faireum-Eric <ericzhang@faireum.io>
2018-12-28 09:34:56 +0800

    finish final version

--- src/components/sections/header/header.js
+++ src/components/sections/header/header.js
@@ -10,4 +10,22 @@ if (!!document.querySelector('.header')) {
   });
 }
 
+$('.header__nav a, .header__mobm-nav a').on('click', function (event) {
+    console.log(this.getAttribute('name'))
+    let target = $(this.getAttribute('name'));
+    console.log(target.offset().top)
 
+    if (target.length) {
+        // event.preventDefault();
+        $('html,body').stop()
+        $('body').animate({
+            scrollTop: target.offset().top + 2
+        }, 500);
+    }
+
+    $('.header__mobm').fadeOut();
+});
+
+$(document).ready(
+  mobileMenu('.header__burger', '.header__mobm', '.header__mobm-close')
+);
